ca1e3db4d686ae22b53fab64971556724423c922
Barbara Filipowska <barbarafilipowska@gmail.com>
2020-09-06 10:52:54 +0200

    Initial Commit

---
+++ Testingthegame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Testingthegame
+//
+//  Created by Barbara Filipowska on 06/09/2020.
+//  Copyright © 2020 Barbara Filipowska. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
